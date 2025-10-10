7c113a7dd791217f4419173424fe9401fdd0fadd
yousefshamaelh-blip <yousef.shamaelh@gmail.com>
2025-10-10 14:27:05 -0400

    adding anchor to index.php and adding other lesson files

--- lesson-6/index.php
+++ lesson-6/index.php
@@ -59,7 +59,7 @@
 				<figure>
 					<img src="images/mini-figure.png" width="176" height="285" alt="Mini figure">
 <!-- STEP 1a: Build an A (anchor) element that surrounds the word 'LEGO®' inside the FIGCAPTION below -->
-					<figcaption>LEGO® mini figure by Vanja (Public Domain)</figcaption>
+					<figcaption><a href ="https://www.lego.com/">LEGO®</a> mini figure by Vanja (Public Domain)</figcaption>
 					<!-- Mini figure by Vanja (Public Domain) https://openclipart.org/detail/224315/clip-is-a-brick-minifig-4 -->
 <!-- STEP 1b: Add an href attribute with the value set to the URL, "https://www.lego.com/" - this is an absolute URL -->
 <!-- STEP 1c: Since the link is pointing to somewhere other than this Web site, it is an external URL - as such, add rel="external" to the above A element start tag -->
